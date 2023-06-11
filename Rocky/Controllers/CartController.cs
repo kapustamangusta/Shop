58b537159048786d09f3651e43c56b195e3db61a
Artem_Sochinskiy <105278734+kapustamangusta@users.noreply.github.com>
2023-06-11 10:46:40 +0300

    Добавил и реализовал метод Summary в контроллере CartController в

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
+using System.Security.Claims;
 
 namespace Rocky.Controllers
 {
@@ -11,7 +13,8 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly AppDBContext _db;
-
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
         public CartController(AppDBContext db)
         {
             _db = db;
@@ -21,7 +24,7 @@ namespace Rocky.Controllers
 
             List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
             if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
-                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count()>0)
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
             {
                 shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
             }
@@ -29,6 +32,39 @@ namespace Rocky.Controllers
             List<int> prodInCart = shoppingCartList.Select(u => u.ProductId).ToList();
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             return View(prodList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(u => u.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == claim.Value)
+            };
+
+
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)
