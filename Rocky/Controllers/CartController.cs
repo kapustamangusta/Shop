cbb54b03df2f77652367d0c4766b30d243f0a60a
Artem_Sochinskiy <105278734+kapustamangusta@users.noreply.github.com>
2023-06-11 09:37:40 +0300

    Добавил и реализовал метод Remove в контроллере CartController

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -28,5 +28,21 @@ namespace Rocky.Controllers
             IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
             return View(prodList);
         }
+
+        public IActionResult Remove(int id)
+        {
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+           
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
